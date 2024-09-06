4781098793656ce61f6f6cd411816fd091db2e69
Johan E Cano Ruiz <johan.memincho@gmail.com>
2024-09-05 23:57:54 -0500

    Create single styles ok

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import Edit from '../img/edit.png'
+import Delete from '../img/delete.png'
+import { Link } from 'react-router-dom'
 
 const Single = () => {
   return (
-    <div>
-      Single
+    <div className='single'>
+      <div className="content">
+        <img src="" alt="" />
+        <div className="user">
+          <img src="" alt="" />
+          <div className="info">
+            <span>Johan</span>
+            <p>Publicado hace 2 días</p>
+          </div>
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt="edit" />
+            </Link>            
+            <img src={Delete} alt="delete" />
+          </div>
+        </div>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel veritatis iste sit, possimus temporibus, soluta tempore, alias deleniti aut quo velit numquam labore nam blanditiis illo eligendi nemo! Ipsa, tenetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti libero necessitatibus vitae, magni culpa dignissimos veritatis vero ea iste quibusdam minus similique animi? Cumque ab deleniti in et, impedit nobis.
+          <br /><br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel veritatis iste sit, possimus temporibus, soluta tempore, alias deleniti aut quo velit numquam labore nam blanditiis illo eligendi nemo! Ipsa, tenetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti libero necessitatibus vitae, magni culpa dignissimos veritatis vero ea iste quibusdam minus similique animi? Cumque ab deleniti in et, impedit nobis.
+          <br /><br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel veritatis iste sit, possimus temporibus, soluta tempore, alias deleniti aut quo velit numquam labore nam blanditiis illo eligendi nemo! Ipsa, tenetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti libero necessitatibus vitae, magni culpa dignissimos veritatis vero ea iste quibusdam minus similique animi? Cumque ab deleniti in et, impedit nobis.
+          <br /><br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel veritatis iste sit, possimus temporibus, soluta tempore, alias deleniti aut quo velit numquam labore nam blanditiis illo eligendi nemo! Ipsa, tenetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti libero necessitatibus vitae, magni culpa dignissimos veritatis vero ea iste quibusdam minus similique animi? Cumque ab deleniti in et, impedit nobis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel veritatis iste sit, possimus temporibus, soluta tempore, alias deleniti aut quo velit numquam labore nam blanditiis illo eligendi nemo! Ipsa, tenetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti libero necessitatibus vitae, magni culpa dignissimos veritatis vero ea iste quibusdam minus similique animi? Cumque ab deleniti in et, impedit nobis.
+        </p>
+      </div>
+      <div className="menu">m</div>
     </div>
   )
 }
