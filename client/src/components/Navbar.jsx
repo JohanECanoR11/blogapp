3d59e154c70783bf13b959b9bb157e604e63883c
Johan E Cano Ruiz <johan.memincho@gmail.com>
2024-09-05 22:46:23 -0500

    Configure navbar styles ok

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../img/BLOGAPP.png'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='navbar'>
+      <div className="container">
+        <div className="logo">
+          <img src={Logo} alt="logo_blogapp" />
+        </div>
+        <div className="links">
+          <Link className='link' to="/?cat=art">
+            <h6>ARTE</h6>
+          </Link>
+          <Link className='link' to="/?cat=art">
+            <h6>CIENCIA</h6>
+          </Link>
+          <Link className='link' to="/?cat=art">
+            <h6>TECNOLOGÍA</h6>
+          </Link>
+          <Link className='link' to="/?cat=art">
+            <h6>CINE</h6>
+          </Link>
+          <Link className='link' to="/?cat=art">
+            <h6>DISEÑO</h6>
+          </Link>
+          <Link className='link' to="/?cat=art">
+            <h6>COMIDA</h6>
+          </Link>
+          <span>Johan</span>
+          <span>Cerrar Sesión</span>
+          <span className='write'>
+            <Link className='link' to="/write">Publicar</Link>
+          </span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
