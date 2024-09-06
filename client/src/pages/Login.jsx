2f3770c8ec8a360e13c61570e84808cbd9828a4c
Johan E Cano Ruiz <johan.memincho@gmail.com>
2024-09-05 22:21:35 -0500

    Configure Register page styles ok

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const Login = () => {
   return (
     <div  className='auth'>
-      <h1>Login</h1>
+      <h1>Ingresar</h1>
       <form>
-        <input type="text" placeholder='Nombre de usuario' />
-        <input type="password" placeholder='Contraseña' />
+        <input required type="text" placeholder='Nombre de usuario' />
+        <input required type="password" placeholder='Contraseña' />
         <button>Ingresar</button>
         <p>mensaje error prueba</p>
         <span>¿No tienes una cuenta?<Link to='/register'>Registrarse</Link></span>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
-    <div>
-      Register
+    <div  className='auth'>
+      <h1>Registrarse</h1>
+      <form>
+        <input required type="text" placeholder='Nombre de usuario' />
+        <input required type="email" placeholder='Correo electrónico' />
+        <input required type="password" placeholder='Contraseña' />
+        <button>Ingresar</button>
+        <p>mensaje error prueba</p>
+        <span>¿Tienes una cuenta?<Link to='/login'>Ingresar</Link></span>
+      </form>
     </div>
   )
 }
