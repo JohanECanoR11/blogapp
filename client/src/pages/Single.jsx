81a0847e7b43eb0fde6ca3c7709c781b97435935
Johan E Cano Ruiz <johan.memincho@gmail.com>
2024-09-06 21:44:01 -0500

    Create Menu component with styles ok

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Edit from '../img/edit.png'
 import Delete from '../img/delete.png'
 import { Link } from 'react-router-dom'
+import Menu from '../components/Menu'
 
 const Single = () => {
   return (
@@ -9,7 +10,8 @@ const Single = () => {
       <div className="content">
         <img src="" alt="" />
         <div className="user">
-          <img src="" alt="" />
+          {/* <img src="" alt="" /> */}
+          <img src="https://bootdey.com/img/Content/avatar/avatar5.png"  alt="" class="superbox-img"/>
           <div className="info">
             <span>Johan</span>
             <p>Publicado hace 2 días</p>
@@ -32,9 +34,9 @@ const Single = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel veritatis iste sit, possimus temporibus, soluta tempore, alias deleniti aut quo velit numquam labore nam blanditiis illo eligendi nemo! Ipsa, tenetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti libero necessitatibus vitae, magni culpa dignissimos veritatis vero ea iste quibusdam minus similique animi? Cumque ab deleniti in et, impedit nobis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel veritatis iste sit, possimus temporibus, soluta tempore, alias deleniti aut quo velit numquam labore nam blanditiis illo eligendi nemo! Ipsa, tenetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti libero necessitatibus vitae, magni culpa dignissimos veritatis vero ea iste quibusdam minus similique animi? Cumque ab deleniti in et, impedit nobis.
         </p>
       </div>
-      <div className="menu">m</div>
+      <Menu />
     </div>
-  )
-}
+  );
+};
 
 export default Single
