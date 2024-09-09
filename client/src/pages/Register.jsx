ed5b4f21347487ce51b31036dba8f8d251e3cbe3
Johan E Cano Ruiz <johan.memincho@gmail.com>
2024-09-08 19:47:06 -0500

    Configuring and installing mysql2 dependence for register page ok

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from "axios"
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username:"",
+    email:"",
+    password:"",
+  })
+
+  const [err, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = e => {
+    setInputs(prev => ({...prev, [e.target.name]: e.target.value}))
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    try {
+      await axios.post("/auth/register", inputs);
+      navigate("/login")
+    }catch(err){
+      setError(err.response.data);
+    }
+  }
+
   return (
     <div  className='auth'>
       <h1>Registrarse</h1>
       <form>
-        <input required type="text" placeholder='Nombre de usuario' />
-        <input required type="email" placeholder='Correo electrónico' />
-        <input required type="password" placeholder='Contraseña' />
-        <button>Ingresar</button>
-        <p>mensaje error prueba</p>
+        <input required type="text" placeholder='Nombre de usuario' name='username' onChange={handleChange} />
+        <input required type="email" placeholder='Correo electrónico' name='email' onChange={handleChange} />
+        <input required type="password" placeholder='Contraseña' name='password' onChange={handleChange} />
+        <button onClick={handleSubmit}>Registrarse</button>
+        {err && <p>{err}</p>}
         <span>¿Tienes una cuenta?<Link to='/login'>Ingresar</Link></span>
       </form>
     </div>
