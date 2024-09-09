5e17bf16c2ffab35bdf90901c2f4af2adbe811eb
Johan E Cano Ruiz <johan.memincho@gmail.com>
2024-09-08 23:21:39 -0500

    Configuring Upload a file to server ok

--- api/index.js
+++ api/index.js
@@ -9,10 +9,27 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use("/api/auth", authRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/posts", postRoutes)
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../client/public/upload')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now()+file.originalname)
+  }
+})
+
+const upload = multer({ storage });
+
+app.post('/api/upload', upload.single('file'), function (req, res) {
+  const file = req.file
+  res.status(200).json(file.filename);
+})
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(8800, () => {
-  console.log("¡Servidor ejecutándose!")
+  console.log("¡Servidor ejecutándose!");
 })
